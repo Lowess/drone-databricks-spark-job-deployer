49a1ce4c74a87d3df18e43835c10f6afed38388f
Florian Dambrine <florian@gumgum.com>
2023-05-26 11:10:19 +0200

    Add spark_env_vars environment variable interpolation

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
 .PHONY: plugin release
 
 VERSION ?= latest
+REPO ?= lowess/drone-databricks-spark-job-deployer
 
 plugin:
 	@echo "Building Drone plugin (export VERSION=<version> if needed)"
-	docker build . -t lowess/drone-databricks-spark-job-deployer:$(VERSION)
+	docker buildx build --platform linux/amd64 . -t $(REPO):$(VERSION)
 
 	@echo "\nDrone plugin successfully built! You can now execute it with:\n"
 	@sed -n '/docker run/,/drone-databricks-spark-job-deployer/p' README.md
 
 release:
 	@echo "Pushing Drone plugin to the registry"
-	docker push lowess/drone-databricks-spark-job-deployer:$(VERSION)
+	docker buildx build --platform linux/amd64 . -t $(REPO):$(VERSION) --push
--- plugin/main.py
+++ plugin/main.py
@@ -3,6 +3,7 @@
 """Deploy a Spark Job to Databricks."""
 
 import json
+import os
 import sys
 from json import JSONDecodeError
 from typing import List
@@ -39,10 +40,13 @@ class DatabricksSparkJobDeployer:
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self._api_token}",
         }
+        response = None
         if method == "GET":
-            return requests.get(endpoint, headers=headers).json()
+            response = requests.get(endpoint, headers=headers)
         elif method == "POST":
-            return requests.post(endpoint, headers=headers, data=data).json()
+            response = requests.post(endpoint, headers=headers, data=data)
+        response.raise_for_status()
+        return response.json()
 
     def get_job_ids(self, cluster_name: str) -> List[str]:
         """
@@ -71,6 +75,25 @@ class DatabricksSparkJobDeployer:
         return self._call_job_api(
             method="POST", action="run-now", data=json.dumps(data)
         )
+
+    def expand_environment_variables(self, env_vars_map):
+        expanded_env_vars = {}
+        for k, v in env_vars_map.items():
+            if v[0:1] == "$" and v[1:].isupper():
+                try:
+                    expanded_env_vars[k] = os.environ[v[1:]]
+                    logger.info(
+                        f"{k} was expanded successfully from environment variable {v}"
+                    )
+                except KeyError:
+                    raise DatabricksInvalidJobSettings(
+                        f"Access to undefined environment variable {v[1:]} while parsing spark_env_vars. "
+                        "The variable should be added to the plugin environment definition to be accessible."
+                    )
+
+            else:
+                expanded_env_vars[k] = v
+        return expanded_env_vars
 
     def configure_job_settings(self, **settings):
         try:
@@ -103,6 +126,7 @@ def main():
     try:
         workspace = dronecli.get("PLUGIN_WORKSPACE")
         api_token = dronecli.get("PLUGIN_API_TOKEN")
+        dry_run = dronecli.get("PLUGIN_DRY_RUN", False)
 
         # Spark job name
         job_name = dronecli.get("PLUGIN_JOB_NAME")
@@ -128,12 +152,24 @@ def main():
             f"The job will be submited with the following settings:\n{json.dumps(job_settings, indent=4)}"
         )
 
+        # Expand env vars stored in spark_env_vars
+        job_settings["new_cluster"][
+            "spark_env_vars"
+        ] = plugin.expand_environment_variables(
+            job_settings["new_cluster"].get("spark_env_vars", {})
+        )
+
         job_ids = plugin.get_job_ids(cluster_name=job_name)
         job_id = None
+
         if len(job_ids) == 0:
             # Job Creation
             logger.info(f"No job '{job_name}' found in cluster. Creating new job...")
-            job_id = plugin.create_job(settings=job_settings)
+
+            if dry_run:
+                logger.info("[dry run] create_job would have been exectued")
+            else:
+                job_id = plugin.create_job(settings=job_settings)
 
         elif len(job_ids) == 1:
             # Job Reset
@@ -141,7 +177,16 @@ def main():
                 f"Found job ID {job_ids} in cluster matching '{job_name}'. Resetting job with new settings..."
             )
             job_id = job_ids[0]
-            response = plugin.reset_job(job_id=job_id, settings=job_settings)
+
+            response = None
+            if dry_run:
+                logger.warning(
+                    f"[dry run] reset_job would have been exectued on job {job_id}"
+                )
+                response = {}
+            else:
+                response = plugin.reset_job(job_id=job_id, settings=job_settings)
+
             if response != {}:
                 raise DatabricksSparkJobDeployerException(
                     f"Resetting old run failed ({response}) ! New run will not start!"
@@ -151,11 +196,16 @@ def main():
                 f"More than one job named '{job_name}' found in cluster: {job_ids}. Aborting deployment..."
             )
 
-        run_job_response = plugin.run_job(job_id=job_id)
+        if dry_run:
+            logger.warning(
+                f"[dry run] run_job would have been exectued on job {job_id}"
+            )
+        else:
+            run_job_response = plugin.run_job(job_id=job_id)
 
-        logger.info(
-            f"New run has been scheduled for {job_id}, run details: {run_job_response}"
-        )
+            logger.info(
+                f"New run has been scheduled for {job_id}, run details: {run_job_response}"
+            )
 
     except Exception as e:
         logger.error(f"{type(e).__name__} error raised while executing the plugin: {e}")
